94cb28da97e68ab1e0dc492ec94c9184e300ec57
JENNYFER PROAÑO <159667610+Klayre27@users.noreply.github.com>
2024-06-30 10:36:29 -0500

    Add files via upload

---
+++ semana5055.py
@@ -0,0 +1,15 @@
+# Programa para calcular el área de un rectángulo
+
+# Definición de variables
+base = 10.5        # Longitud de la base del rectángulo en metros
+altura = 4.8       # Altura del rectángulo en metros
+nombre_figura = "Rectángulo"
+
+# Cálculo del área
+area = base * altura
+
+# Impresión del resultado
+print(f"Calculando el área de la figura {nombre_figura}:")
+print(f"Base: {base} metros")
+print(f"Altura: {altura} metros")
+print(f"El área del {nombre_figura} es: {area} metros cuadrados")
